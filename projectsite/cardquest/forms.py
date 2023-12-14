235152632d49849cdfd5c807f20fce9543d6815d
milkygalicia9 <202080177@psu.palawan.edu.ph>
2023-12-14 23:17:58 +0800

    done changes

---
+++ projectsite/cardquest/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from django import forms
+from .models import Trainer, Collection, PokemonCard
+
+class TrainerForm(ModelForm):
+    birthdate = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    
+    class Meta:
+        model = Trainer
+        fields = "__all__"
+
+class CollectionForm(ModelForm):
+    collection_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    class Meta:
+        model = Collection
+        fields = "__all__"
